d6449f38c942243282e277a3a89b57de3fd1c198
Sergey Ukhlivanov <ukhlivanov@gmail.com>
2018-08-14 15:35:49 -0700

    Added tests

---
+++ quoka.js
@@ -0,0 +1,6 @@
+const faker = require('faker');
+console.log(faker.name.firstName());
+console.log(faker.name.lastName());
+console.log(faker.lorem.text());
+console.log(faker.name.title());
+console.log(faker.date.past());
